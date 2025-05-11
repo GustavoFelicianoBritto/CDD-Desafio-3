44caf9b2c126c34d1b4f5d0b673773ac3d525f39
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-05-11 19:43:01 -0300

    Optimização, formatação, nova função menu dentro da classe

--- library.py
+++ library.py
@@ -1,23 +1,54 @@
-class tarefasClass():
+class TarefasClass():
     def __init__(self):
-        pass
+        self.listaClasse=[]
+        self.tempTarefa = ""
 
-    def listarTarefas(self,lista):
-        print("Lista de tarefas: ")
-        for index,item in enumerate(lista):
-            print(index, lista[index])
+    def listartarefas(self):
+        if self.listaClasse:
+            print("Lista de tarefas: ")
+            for index,item in enumerate(self.listaClasse):
+                print(f"{index} - {self.listaClasse[index]}")
+            print()
+        else:
+            print("Lista se encontra Vazia")
+
+    def inserirtarefas(self):
+        temptarefa=input("Insira uma tarefa: ")
+        self.listaClasse.append(temptarefa)
+        print(f"Tarefa {temptarefa} registrada.")
         print()
 
-    def inserirTarefas(self,lista):
-        tempTarefa=input("Insira uma tarefa: ")
-        lista.append(tempTarefa)
-
-    def deletarTarefas(self,lista):
-        optionDelete=None
-        print("Qual tarefa deseja deletar? (numero)")
-        for index,item in enumerate(lista):
-            print(index, lista[index])
-        optionDelete= int(input())
-        lista.pop(optionDelete)
+    def deletartarefas(self):
+        optiondelete=None
+        if self.listaClasse:
+            print("Qual tarefa deseja deletar? (numero)")
+            self.listartarefas()
+            optiondelete= int(input())
+            self.tempTarefa=self.listaClasse[optiondelete]
+            self.listaClasse.pop(optiondelete)
+            print(f"Tarefa {self.tempTarefa} deletada.")
 
 
+        else:
+            print("Lista se encontra vazia")
+
+
+    def menu(self):
+        option=0
+        while True:
+            option = int(
+                input("\n1- listar tarefas \n2- inserir uma tarefa \n3- deletar tarefa\n4- sair\nEscolha uma opção:\n"))
+            print()
+            print()
+            if option == 4:
+                print("Programa finalizado, Lista final:")
+                self.listartarefas()
+                break
+            elif option == 1:
+                self.listartarefas()
+            elif option == 2:
+                self.inserirtarefas()
+            elif option == 3:
+                self.deletartarefas()
+            else:
+                print("Opção inválida")
--- main.py
+++ main.py
@@ -1,29 +1,6 @@
-from library import tarefasClass
-# menu
-#
-# 1- listar tarefasClass
-# 2- inserir uma tarefa
-# 3- deletar tarefa
-# 4- sair
+from library import TarefasClass
 
-gerenciadorDeTarefas = tarefasClass()
+gerenciadorDeTarefas = TarefasClass()
 
-tarefasLista=["teste1","teste2","teste3"]
-option=0
-
-while True:
-    option = int(input("Escolha uma opção:\n\n1- listar tarefas \n2- inserir uma tarefa \n3- deletar tarefa\n4- sair"))
-    print()
-    print()
-    if option==4:
-        print("Programa finalizado")
-        break
-    elif option==1:
-        gerenciadorDeTarefas.listarTarefas(tarefasLista)
-    elif option==2:
-        gerenciadorDeTarefas.inserirTarefas(tarefasLista)
-    elif option==3:
-        gerenciadorDeTarefas.deletarTarefas(tarefasLista)
-    else:
-        print("Opção inválida")
+gerenciadorDeTarefas.menu()
 
